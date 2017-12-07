9e1c41f5c676a0121d66bb658d1bf83f146a0b52
Naomi Pohl <naomi@Naomis-Air.hotwirephone.com>
2017-12-07 18:59:28 -0500

    change some sql statements

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,8 @@ module PostsHelper
   end
 
   def tag_cloud(tags, classes)
-    max = tags.sort_by(&:count).last
+    max = tags.order(&:count).last
+    # max = tags.sort_by(&:count).last
     tags.each do |tag|
       index = tag.count.to_f / max.count * (classes.size-1)
       yield(tag, classes[index.round])
