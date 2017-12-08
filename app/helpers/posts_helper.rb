d0467376c519c59e9ecf57f2adb4c6390dd9224e
Naomi Pohl <naomi@Naomis-Air.hotwirephone.com>
2017-12-07 19:29:13 -0500

    changes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,12 +3,12 @@ module PostsHelper
     tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
   end
 
-  def tag_cloud(tags, classes)
-    max = tags.order(&:count).last
-    # max = tags.sort_by(&:count).last
-    tags.each do |tag|
-      index = tag.count.to_f / max.count * (classes.size-1)
-      yield(tag, classes[index.round])
-    end
-  end
+  #def tag_cloud(tags, classes)
+    #max = tags.order(&:count).last
+    #max = tags.sort_by(&:count).last
+    #tags.each do |tag|
+      #index = tag.count.to_f / max.count * (classes.size-1)
+      #yield(tag, classes[index.round])
+    #end
+  #end
 end
