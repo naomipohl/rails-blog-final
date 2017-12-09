955faad96decf7cdc8d697222d473892969936d4
Naomi Pohl <naomi@android-b1b02812b97196fa.localdomain>
2017-12-09 18:54:04 -0500

    trying again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,3 @@
-if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -9,4 +8,3 @@ if Rails.env.production?
     }
     config.fog_directory  = ENV['S3_BUCKET']
   end
-end
