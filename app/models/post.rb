cf5356f62e35385baf5a150e056050d3f6495638
Naomi Pohl <naomi@vagelos-ve509-0483.apn.wlan.upenn.edu>
2017-12-10 15:11:58 -0500

    fix tag cloud

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,8 +15,9 @@ class Post < ApplicationRecord
   end
 
   def self.tag_counts
-    Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).group('taggings.tag_id')
-  end
+    Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").
+      joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
+  end  
 
   def self.tagged_with(name)
     Tag.find_by_name!(name).posts
