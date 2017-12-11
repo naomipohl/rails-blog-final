484a7a689cb4baa13b669a1d9122be182fcbeb7e
Naomi Pohl <naomi@Naomis-Air.hotwirephone.com>
2017-12-11 00:15:14 -0500

    fix buggy email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def reset_password_instructions(admin)
+    @admin = admin
+    mail( :to => @admin.email,
+    :subject => 'Reset password instructions' )
+  end
 end
