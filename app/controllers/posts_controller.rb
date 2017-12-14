f10e5433a6d625ef108281fb2d764b66f7d179da
Naomi Pohl <naomi@Naomis-Air.hotwirephone.com>
2017-12-13 20:17:17 -0500

    add file_name to images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :admin_id, :all_tags, :image)
+    params.require(:post).permit(:title, :body, :admin_id, :all_tags, :image, :image_file_name)
   end
 
   def find_post
--- db/migrate/20171214010104_add_image_to_posts.rb
+++ db/migrate/20171214010104_add_image_to_posts.rb
@@ -1,11 +1,12 @@
 class AddImageToPosts < ActiveRecord::Migration[5.1]
   def change
-	  def self.up
-	    add_attachment :posts, :image
-	  end
+    add_column :posts, :file_name, :string
+	def self.up
+	  add_attachment :posts, :image
+	end
 
-	  def self.down
-	    remove_attachment :posts, :image
-	  end
+	def self.down
+	  remove_attachment :posts, :image
+	end
   end
 end
