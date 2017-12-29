006ca44ab5e4f37ff6d1ca8c90bfa233079f21f9
Naomi Pohl <naomi.pohl@yahoo.com>
2017-12-29 17:20:18 -0500

    change app.rb file

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,8 @@ Bundler.require(*Rails.groups)
 
 module Blog2
   class Application < Rails::Application
-    config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
-    config.assets.enabled = true
     config.assets.precompile += Ckeditor.assets
-	config.assets.precompile += %w(ckeditor/*)
-	config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
